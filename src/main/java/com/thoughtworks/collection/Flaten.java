9fa1bf03d0c4db344fca359db96a3deba1bae104
GRAY JEANNE MEDINA (SHP-IRIS-ISD-OOCLL/MNL) <MEDINGR2@OOCL.COM>
2019-10-12 14:27:49 +0800

    finish Flaten.

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,11 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Flaten {
 
@@ -16,23 +12,10 @@ public class Flaten {
     }
 
     public List<Integer> transformToOneDimesional() {
-        Integer[] flatArray = flattenStream(array)
-                .toArray(Integer[] ::new);
-       return Arrays.asList(flatArray);
+        throw new NotImplementedException();
+    }
 
-    }
-    public static <T> Stream<T> flattenStream(T[][] arrays)
-    {
-        List<T> list = new ArrayList<>();
-        for (T[] array : arrays) {
-            Arrays.stream(array)
-                    .forEach(list::add);
-        }
-        return list.stream();
-    }
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        Integer[] flatArray = flattenStream(array)
-                .toArray(Integer[] ::new);
-        return Arrays.asList(flatArray).stream().distinct().collect(Collectors.toList());
+        throw new NotImplementedException();
     }
 }
